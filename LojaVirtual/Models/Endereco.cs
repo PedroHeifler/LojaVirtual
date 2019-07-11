797b2ca083be4386dff0b359eb20e29291e404ad
Estagiario 011 <stag011@benner.com.br>
2019-07-11 11:53:23 -0300

    Funcionamento Basico do Cadastro de Usuario

---
+++ LojaVirtual/Models/Endereco.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LojaVirtual.Models
+{
+    public class Endereco
+    {
+        [Key, Column(Order = 1)]
+        public int IdEndereco { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+    }
+}
---
+++ LojaVirtual/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LojaVirtual.Models
+{
+    public class Login
+    {
+        [Key, Column(Order = 1)]
+        public int IdLogin { get; set; }
+        public String Email { get; set; }
+        public String Senha { get; set; }
+        public String TpUsuario { get; set; }
+    }
+}
