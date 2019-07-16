1c3b7276e9e69c85f8a4d3e238d6fefd1e55d7fd
Estagiario 011 <stag011@benner.com.br>
2019-07-16 09:35:37 -0300

    Filtro incompleto

---
+++ LojaVirtual/Scripts/Filtro.js
@@ -0,0 +1,24 @@
+﻿var campoFiltro = $("#filtro-tabela").on("input onpropertychange", function () {
+    var usuarios = $(".usuario");
+
+    if (this.value.length > 0) {
+        for (var i = 0; i < usuarios.length; i++) {
+            var usuario = usuarios[i];
+            var tdNome = usuario.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
+            console.log(nome)
+
+            if (expressao.test(nome)) {
+                usuario.classList.add("invisivel");
+            } else {
+                usuario.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (var i = 0; i < usuarios.length; i++) {
+            var usuario = usuarios[i];
+            usuario.classList.remove("invisivel");
+        }
+    }
+});
