20ae4f3f5f60694359e690f2bdca10e99aea0c89
Estagiario 011 <stag011@benner.com.br>
2019-07-23 08:34:49 -0300

    Filtrar

--- LojaVirtual/Scripts/Filtro.js
+++ LojaVirtual/Scripts/Filtro.js
@@ -1,24 +1,27 @@
-﻿var campoFiltro = $("#filtro-tabela").on("input onpropertychange", function () {
-    var usuarios = $(".usuario");
+﻿function Filtrar(seletor) {
+    
+    $("#filtro-tabela").on("input onpropertychange", function () {
+        var usuarios = $(seletor);
 
-    if (this.value.length > 0) {
-        for (var i = 0; i < usuarios.length; i++) {
-            var usuario = usuarios[i];
-            var tdNome = usuario.querySelector(".info-nome");
-            var nome = tdNome.textContent;
-            var expressao = new RegExp(this.value, "i");
-            console.log(nome)
+        if (this.value.length > 0) {
+            for (var i = 0; i < usuarios.length; i++) {
+                var usuario = usuarios[i];
+                var tdNome = usuario.querySelector(".info-nome");
+                var nome = tdNome.text();
+                var expressao = new RegExp(this.value, "i");
+                console.log(nome)
 
-            if (!expressao.test(nome)) {
-                usuario.classList.add("invisivel");
-            } else {
+                if (!expressao.test(nome)) {
+                    usuario.classList.add("invisivel");
+                } else {
+                    usuario.classList.remove("invisivel");
+                }
+            }
+        } else {
+            for (var i = 0; i < usuarios.length; i++) {
+                var usuario = usuarios[i];
                 usuario.classList.remove("invisivel");
             }
         }
-    } else {
-        for (var i = 0; i < usuarios.length; i++) {
-            var usuario = usuarios[i];
-            usuario.classList.remove("invisivel");
-        }
-    }
-});
+    });
+}
