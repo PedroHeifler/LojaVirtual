d39e8fa7e428c483a6916743bca81035ad724795
Estagiario 011 <stag011@benner.com.br>
2019-08-12 11:53:58 -0300

    Carrinho Fazendo

--- LojaVirtual/Scripts/PassaParaCarrinho.js
+++ LojaVirtual/Scripts/PassaParaCarrinho.js
@@ -1,6 +1,14 @@
-﻿function PassarCarrinho(nome) {
-    for (var i = 0; i < nome.length; i++) {
-        sessionStorage.setItem("nomes", nome[i]);
-    }
+﻿function PassarCarrinho(produto) {
+
+    var produtos = sessionStorage.getItem("produto")
     
+    if (produtos == null) {
+        sessionStorage.setItem("produto"+ [i], produto);
+    }
+
+    if (produtos != null) {
+        for (var i = 0; i < produtos.length + 1; i++) {
+            sessionStorage.setItem("produto"+[i], produto);
+        }
+    }
 }
