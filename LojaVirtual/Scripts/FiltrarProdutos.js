eed14fcbc2ed7a7c99ba931824f867eed5680de4
Pedro Henrique <pedroheifler@hotmail.com>
2019-08-26 17:04:30 -0300

    Categoria fazendo

---
+++ LojaVirtual/Scripts/FiltrarProdutos.js
@@ -0,0 +1,16 @@
+﻿function FiltrarProdutos(genero) {
+    const generoCheck = $(genero).val()
+    let campo = $(".filtro-produtos").children(".card-body").children("h5")
+
+    var expressao = new RegExp(generoCheck, "i");
+
+    for (var i = 0; i < campo.length; i++) {
+
+        var a = $(".filtro-produtos")
+        if (!expressao.test(campo[i])) {
+            a.addClass("invisivel");
+        } else {
+            a.removeClass("invisivel");
+        }
+    }
+}
